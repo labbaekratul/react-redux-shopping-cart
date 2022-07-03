813a8a923f678ac4d9d643f166a8209e9c2ae4cb
labbaek <labbaekratul@gmail.com>
2022-07-04 01:28:03 +0600

    cart and product details route added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import Home from "./pages/Home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap-grid.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+import Cart from "./pages/Cart/Cart";
+import ProductDetails from "./pages/ProductDetails/ProductDetails";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
       </Routes>
     </BrowserRouter>
   );
